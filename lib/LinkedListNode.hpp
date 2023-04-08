db0d8cd58ea6499ff29eca0d79c7fc3f1bb65662
HazyFish <hazyfish@outlook.com>
2023-04-08 13:49:33 -0700

    feat: allow mutation to Node value

--- lib/LinkedListNode.hpp
+++ lib/LinkedListNode.hpp
@@ -7,19 +7,19 @@ template <typename T>
 class LinkedListNode
 {
 private:
-    // make constructors and fields only available to LinkedList class.
+    // make constructors and `_next` field only available to `LinkedList` class
+    // to avoid instantiating node and mutating `_next` outside `LinkedList` class.
 
     template <typename U>
     friend class LinkedList;
 
-    T _value;
     LinkedListNode<T> *_next;
 
     explicit LinkedListNode(T value) : _value(value), _next(nullptr) {}
     LinkedListNode(T value, LinkedListNode<T> *next) : _value(value), _next(next) {}
 
 public:
-    T value() { return _value; }
+    T value;
     LinkedListNode<T> *next() { return _next; }
 };
 
--- tests/LinkedListTest.cpp
+++ tests/LinkedListTest.cpp
@@ -4,30 +4,36 @@
 TEST(LinkedListTest, InstantiateEmptyList) {
     LinkedList<int> llist;
 
-    ASSERT_EQ(llist.size(), 0);
     ASSERT_EQ(llist.head(), nullptr);
     ASSERT_EQ(llist.tail(), nullptr);
+    ASSERT_EQ(llist.size(), 0);
 }
 
 TEST(LinkedListTest, AppendToEmptyList) {
     LinkedList<int> llist;
-
     LinkedListNode<int> *node = llist.append(17);
+
+    ASSERT_EQ(node, llist.head());
     ASSERT_EQ(node, llist.tail());
+    ASSERT_EQ(node->value, 17);
+    ASSERT_EQ(node->next(), nullptr);
     ASSERT_EQ(llist.size(), 1);
-    ASSERT_EQ(llist.head()->value(), 17);
-    ASSERT_EQ(llist.tail()->value(), 17);
 }
 
 TEST(LinkedListTest, AppendToNonEmptyList) {
     LinkedList<int> llist;
-    llist.append(17);
+    LinkedListNode<int> *first = llist.append(17);
+    LinkedListNode<int> *second = llist.append(19);
 
-    LinkedListNode<int> *node = llist.append(19);
-    ASSERT_EQ(node, llist.tail());
+    ASSERT_EQ(first, llist.head());
+    ASSERT_EQ(first->value, 17);
+    ASSERT_EQ(first->next(), second);
+
+    ASSERT_EQ(second, llist.tail());
+    ASSERT_EQ(second->value, 19);
+    ASSERT_EQ(second->next(), nullptr);
+
     ASSERT_EQ(llist.size(), 2);
-    ASSERT_EQ(llist.head()->value(), 17);
-    ASSERT_EQ(llist.tail()->value(), 19);
 }
 
 // TODO: Add more tests for LinkedList here
